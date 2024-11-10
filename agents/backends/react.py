c17b0bcb39b8d7b8245292f53014fd0c395280f2
hlfshell <kchester@gmail.com>
2024-11-09 16:32:14 -0800

    feature: react follows new pattern set by simple backend

--- agents/backends/react.py
+++ agents/backends/react.py
@@ -1,13 +1,16 @@
 from __future__ import annotations
 
 import json
-from typing import Any, Dict, List, Optional, Union
+import pathlib
+from os import path
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 from pydantic import BaseModel
 
 from agents.backends.base import BaseBackend
-from agents.llms.llm import Prompt
-from agents.tools.tool import ToolArguments, ToolResults
+from agents.llms.llm import LLM, Prompt
+from agents.templater import PromptTemplate
+from agents.tools.tool import Tool, ToolArguments, ToolResults
 
 
 class ReActResponse(BaseModel):
@@ -18,6 +21,23 @@ class ReActResponse(BaseModel):
 
 
 class ReActBackend(BaseBackend):
+
+    def __init__(
+        self,
+        llm: LLM,
+        tools: List[Tool],
+        agent_explanation: str,
+    ):
+        super().__init__(llm, tools, max_simultaneous_tools=1)
+
+        self.agent_explanation = agent_explanation
+        self.__templater = PromptTemplate.from_file(
+            path.join(
+                pathlib.Path(__file__).parent,
+                "prompts",
+                "react.prompt",
+            )
+        )
 
     def __parse(self, text: str) -> ReActResponse:
         lines = text.strip().split("\n")
@@ -37,6 +57,8 @@ class ReActBackend(BaseBackend):
         # Extract Action and Action Input
         while lines:
             line = lines.pop(0)
+            if not line.strip():
+                continue
             if line.startswith("Action:"):
                 results["Action"] = line.split("Action:", 1)[1].strip()
             elif line.startswith("Action Input:"):
@@ -62,7 +84,7 @@ class ReActBackend(BaseBackend):
         # Handle missing Answer if Action is present - necessary for
         # pydantic
         if results["Action"] is not None and results["Answer"] is None:
-            results["Answer"] = ()
+            results["Answer"] = ""
 
         # Convert Action Input to ActionInput for pydantic
         results["ActionInput"] = results["Action Input"]
@@ -76,21 +98,61 @@ class ReActBackend(BaseBackend):
 
     def parse_for_tool_calls(
         self, text: str, stop_at_first_tool: bool = False
-    ) -> Dict[str, ToolArguments]:
+    ) -> List[Tuple[str, ToolArguments]]:
         response = self.__parse(text)
 
-        return {
-            response.Action: response.ActionInput,
-        }
+        return (
+            []
+            if not response.Action
+            else [
+                (
+                    response.Action,
+                    response.ActionInput,
+                )
+            ]
+        )
 
     def tool_results_to_prompts(
         self, prompt: Prompt, results: ToolResults
     ) -> List[Prompt]:
-        out = f"Observation:\nThe {results[0]} function returned the following information:\n"
+        for name, args, result in results:
+            out = f"---\n{name}("
 
-        out += results[2]
+            first_tool = True
+            for arg, value in args.items():
+                if first_tool:
+                    first_tool = False
+                else:
+                    out += ", "
+                out += f"{arg}="
+                if isinstance(value, str):
+                    out += f'"{value}"'
+                else:
+                    out += f"{value}"
+            out += f") returned:\n{result}\n"
+            prompt.append(
+                {
+                    "role": "system",
+                    "content": out,
+                }
+            )
+        return prompt
 
-        return prompt.append({"role": "system", "content": out})
+    def prepare_prompt(self, **kwargs) -> Prompt:
+        # Create the tools block
+        tools_block = ""
+        for _, tool in self.tools.items():
+            tools_block += f"{tool}\n"
+
+        content = self.__templater.render(
+            {
+                # "agent_explanation": self.agent_explanation,
+                "tools_block": tools_block,
+                "task": kwargs["task"],
+            }
+        )
+
+        return [{"role": "system", "content": content}]
 
 
 class FormatException(Exception):
