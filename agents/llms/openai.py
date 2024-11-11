7a2ac0dd86c87b823a4424351be8ccd850a76c23
hlfshell <kchester@gmail.com>
2024-11-10 17:41:36 -0800

    created basic openai llm

---
+++ agents/llms/openai.py
@@ -0,0 +1,35 @@
+import os
+from typing import Optional
+
+import openai
+
+from agents.agent_old import LLM, Prompt
+
+
+class OpenAILLM(LLM):
+    def __init__(
+        self,
+        api_key: Optional[str] = None,
+        model: str = "gpt-3.5-turbo",
+        temperature: float = 0.7,
+        max_tokens: int = 1024,
+    ):
+        self.model = model
+
+        self.__api_key = api_key or os.getenv("OPENAI_API_KEY")
+
+        # self.client = openai.Client(api_key=self.__api_key)
+        self.client = openai.Client(api_key=self.__api_key)
+
+        self.temperature = temperature
+        self.max_tokens = max_tokens
+
+    def completion(self, prompt: Prompt) -> str:
+        response = self.client.chat.completions.create(
+            model=self.model,
+            messages=prompt,
+            max_tokens=self.max_tokens,
+            temperature=self.temperature,
+        )
+
+        return response.choices[0].message.content
