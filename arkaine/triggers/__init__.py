b20fcf70439aca357f39b3e14e7070cc12dcc0c1
hlfshell <kchester@gmail.com>
2025-01-02 14:56:02 -0800

    feature: rss trigger

--- arkaine/triggers/__init__.py
+++ arkaine/triggers/__init__.py
@@ -1,6 +1,7 @@
 from arkaine.triggers.api import API
 from arkaine.triggers.cli import CLI
+from arkaine.triggers.rss import RSS, Feed
 from arkaine.triggers.schedule import Schedule, Task
 from arkaine.utils.interval import Interval
 
-__all__ = ["API", "CLI", "Schedule", "Task", "Interval"]
+__all__ = ["API", "CLI", "Schedule", "Task", "Interval", "RSS", "Feed"]
