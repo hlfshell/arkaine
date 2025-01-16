d46ad2fed43b4b1b866889d34542658928849c64
hlfshell <kchester@gmail.com>
2025-01-16 15:54:31 -0800

    fix: added inbox

--- arkaine/connectors/__init__.py
+++ arkaine/connectors/__init__.py
@@ -1,7 +1,16 @@
-from arkaine.integrations.api import API
-from arkaine.integrations.cli import CLI
-from arkaine.integrations.rss import RSS, Feed
-from arkaine.integrations.schedule import Schedule, Task
+from arkaine.connectors.api import API
+from arkaine.connectors.cli import CLI
+from arkaine.connectors.rss import RSS, Feed
+from arkaine.connectors.schedule import Schedule, Task
 from arkaine.utils.interval import Interval
 
-__all__ = ["API", "CLI", "Schedule", "Task", "Interval", "RSS", "Feed"]
+__all__ = [
+    "API",
+    "CLI",
+    "Schedule",
+    "Task",
+    "Interval",
+    "RSS",
+    "Feed",
+    "Inbox",
+]
