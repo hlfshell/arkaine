5f76b3de033720913cbdc7cec9b2ad39d721f27a
hlfshell <kchester@gmail.com>
2025-01-24 23:02:19 -0800

    feature: quickstart function for global featurex

--- arkaine/__init__.py
+++ arkaine/__init__.py
@@ -8,3 +8,8 @@ with built-in tools and capabilities.
 __version__ = "0.0.3"
 __author__ = "Keith Chester"
 __email__ = "keith@hlfshell.ai"
+
+
+from arkaine.quickstart import quickstart
+
+__all__ = ["quickstart"]
