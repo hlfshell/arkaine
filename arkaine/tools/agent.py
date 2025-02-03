8d8dde3fd478a8cc433a8b6f3961cf50602ed988
Keith Chester <kchester@gmail.com>
2025-02-03 15:53:09 -0800

    feature: Some rework of expectations of agents

--- arkaine/tools/agent.py
+++ arkaine/tools/agent.py
@@ -1,13 +1,29 @@
 from abc import ABC, abstractmethod
 from typing import Any, Callable, Dict, List, Optional
 
-from arkaine.backends.base import BaseBackend
+from arkaine.backends.backend import Backend
 from arkaine.llms.llm import LLM, Prompt
 from arkaine.tools.result import Result
 from arkaine.tools.tool import Argument, Context, Example, Tool
 
 
 class Agent(Tool, ABC):
+    """
+    An agent is a tool that utilizes an LLM. Provide an LLM model to generate
+    completions, and implement prepare_prompt to convert incoming arguments
+    to a prompt for your agent.
+
+    Args:
+        name: The name of the agent.
+        description: The description of the agent for other tools to use.
+        args: The arguments of the agent.
+        llm: The LLM model to use for the agent.
+        examples: The examples of the agent to further explain its usage.
+        result: The result of the agent.
+        id: The id of the agent; generally leave this blank, as a UUID
+            will be generated.
+    """
+
     def __init__(
         self,
         name: str,
@@ -58,6 +74,23 @@ class Agent(Tool, ABC):
 
 
 class SimpleAgent(Agent):
+    """
+    SimpleAgent is a helper class that allows you to create an agent by
+    providing a function to prepare the prompt and a function to extract the
+    result, preventing the need to implement an inheriting agent.
+
+    Args:
+        name: The name of the agent.
+        description: The description of the agent for other tools to use.
+        args: The arguments of the agent.
+        llm: The LLM model to use for the agent.
+        prepare_prompt: A function to prepare the prompt for the agent.
+        extract_result: A function to extract the result from the LLM.
+        examples: The examples of the agent to further explain its usage.
+        id: The id of the agent; generally leave this blank, as a UUID
+            will be generated.
+        result: The result of the agent; optional.
+    """
 
     def __init__(
         self,
@@ -86,6 +119,23 @@ class SimpleAgent(Agent):
 
 
 class IterativeAgent(Agent):
+    """
+    IterativeAgent is an agent that can iterate on a task until a result is
+    found. It will continue to call the LLM with the provided prompt until the
+    result is not None.
+
+    Args:
+        name: The name of the agent.
+        description: The description of the agent for other tools to use.
+        args: The arguments of the agent.
+        llm: The LLM model to use for the agent.
+        examples: The examples of the agent to further explain its usage.
+        result: The result of the agent; optional.
+        initial_state: The initial state of the agent.
+        max_steps: The maximum number of steps the agent can take.
+        id: The id of the agent; generally leave this blank, as a UUID
+            will be generated.
+    """
 
     def __init__(
         self,
@@ -135,6 +185,25 @@ class IterativeAgent(Agent):
 
 
 class SimpleIterativeAgent(IterativeAgent):
+    """
+    SimpleIterativeAgent is a helper class that allows you to create an
+    iterative agent by providing a function to prepare the prompt and a
+    function to extract the result, preventing the need to implement an
+    inheriting agent.
+
+    Args:
+        name: The name of the agent.
+        description: The description of the agent for other tools to use.
+        args: The arguments of the agent.
+        llm: The LLM model to use for the agent.
+        prepare_prompt: A function to prepare the prompt for the agent.
+        extract_result: A function to extract the result from the LLM.
+        examples: The examples of the agent to further explain its usage.
+        id: The id of the agent; generally leave this blank, as a UUID
+            will be generated.
+        result: The result of the agent; optional.
+    """
+
     def __init__(
         self,
         name: str,
@@ -158,29 +227,64 @@ class SimpleIterativeAgent(IterativeAgent):
         return self.__extract_result(context, output)
 
 
-class BackendAgent(Tool, ABC):
+class BackendAgent(Tool):
+    """
+    BackendAgent is an agent that utilizes a backend to execute the agent's
+    task. It will prepare the arguments for the backend and then invoke the
+    backend with the prepared arguments.
+
+    Args:
+        name: The name of the agent.
+        description: The description of the agent for other tools to use.
+        agent_explanation: The explanation of the agent to explain to the
+            LLM what the goal of the agent is.
+        args: The arguments of the agent.
+        backend: The backend to use for the agent.
+        examples: The examples of the agent to further explain its usage.
+        id: The id of the agent; generally leave this blank, as a UUID
+            will be generated.
+        result: The result of the agent; optional.
+    """
 
     def __init__(
         self,
         name: str,
         description: str,
+        agent_explanation: str,
         args: List[Argument],
-        backend: BaseBackend,
+        backend: Backend,
         examples: List[Example] = [],
         result: Optional[Result] = None,
         id: Optional[str] = None,
     ):
         super().__init__(name, description, args, None, examples, id, result)
         self.backend = backend
+        self.agent_explanation = agent_explanation
 
-    @abstractmethod
-    def prepare_for_backend(self, **kwargs) -> Dict[str, Any]:
+    def prepare_for_backend(
+        self,
+        context: Context,
+        **kwargs,
+    ) -> Dict[str, Any]:
         """
         Given the arguments for the agent, transform them
         (if needed) for the backend's format. These will be
-        passed to the backend as arguments.
+        passed to the backend as arguments. By default,
+        the backend agent will pass the arguments as-is to the
+        backend; overwrite this method in your implementation
+        if need be.
+
+        Typically, you want to convert all arguments into:
+
+        {
+            "task": "...",
+        }
+
+        ...wherein the task is a string that describes the current
+        task clearly to the agent given its explanation.
         """
-        pass
+        return kwargs
 
     def invoke(self, context: Context, **kwargs) -> Any:
-        return self.backend.invoke(context, self.prepare_for_backend(**kwargs))
+        prepared_args = self.prepare_for_backend(context, **kwargs)
+        return self.backend.invoke(context, prepared_args)
