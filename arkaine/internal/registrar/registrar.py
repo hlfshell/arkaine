5c8b93a92db36eef3facf07a572ab4d752a4c2cc
Keith Chester <kchester@gmail.com>
2025-02-08 09:09:41 -0800

    feature: update broadcasting in the registrar

--- arkaine/internal/registrar/registrar.py
+++ arkaine/internal/registrar/registrar.py
@@ -1,12 +1,57 @@
 from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
 from threading import Lock
-from typing import TYPE_CHECKING, Callable, Dict, List
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional
 from arkaine.tools.attachable import Attachable
+from arkaine.internal.to_json import recursive_to_json
+from uuid import uuid4
 
 if TYPE_CHECKING:
     from arkaine.llms.llm import LLM
     from arkaine.tools.context import Context
     from arkaine.tools.tool import Tool
+
+
+class Update:
+    def __init__(
+        self, target: str, type: str, data: Any, id: Optional[str] = None
+    ):
+        self.__id = id if id else str(uuid4())
+        self.__target = target
+        self.__type = type
+        self.__data = data
+        self.__time = datetime.now()
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def target(self):
+        return self.__target
+
+    @property
+    def type(self):
+        return self.__type
+
+    @property
+    def data(self):
+        return self.__data
+
+    @property
+    def time(self):
+        return self.__time
+
+    def to_json(self):
+        return recursive_to_json(
+            {
+                "id": self.__id,
+                "target": self.__target,
+                "type": self.__type,
+                "data": self.__data,
+                "time": self.__time,
+            }
+        )
 
 
 class Registrar:
@@ -20,6 +65,9 @@ class Registrar:
     _on_producer_call_listeners: List[
         Callable[[Attachable, "Context"], None]
     ] = []
+
+    __update_listeners: Dict[str, List[Callable[[Update], None]]] = {}
+    __update_listeners["all"] = []
 
     def __new__(cls):
         raise ValueError("Registrar cannot be instantiated")
@@ -39,6 +87,25 @@ class Registrar:
 
             if hasattr(item, "on_call_listener"):
                 item.add_on_call_listener(cls._on_producer_call)
+
+    @classmethod
+    def broadcast_update(cls, update: Update):
+        """Broadcast an update to all registered listeners of that type"""
+        with cls._lock:
+            for listener in cls.__update_listeners["all"]:
+                cls.__executor.submit(listener, update)
+            if update.type in cls.__update_listeners:
+                for listener in cls.__update_listeners[update.type]:
+                    cls.__executor.submit(listener, update)
+
+    @classmethod
+    def add_on_update_listener(
+        cls, type: str, listener: Callable[[Update], None]
+    ):
+        with cls._lock:
+            if type not in cls.__update_listeners:
+                cls.__update_listeners[type] = []
+            cls.__update_listeners[type].append(listener)
 
     @classmethod
     def _on_producer_call(cls, producer: Attachable, ctx: "Context"):
