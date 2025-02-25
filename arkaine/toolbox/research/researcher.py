f861f4279caa219bc46420cf1a3e4d08b2dfe62f
Keith Chester <kchester@gmail.com>
2025-02-25 12:25:21 -0800

    doc: documentation added

--- arkaine/toolbox/research/researcher.py
+++ arkaine/toolbox/research/researcher.py
@@ -51,6 +51,15 @@ class ResourceJudge(AbstractAgent):
 
 
 class DefaultResourceJudge(ResourceJudge):
+    """
+    DefaultResourceJudge is a tool that is utilized to judge a list of resources
+    based on a given query/topic/task. It is fed a query/topic/task and a list
+    of resources and their descriptions, and it determines which of those
+    resources are likely to contain useful information.
+
+    Args:
+        llm (LLM): The LLM to use
+    """
 
     def __init__(self, llm: LLM):
         super().__init__(
@@ -195,11 +204,41 @@ class FindingsGenerator(AbstractAgent):
 
 
 class GenerateFinding(FindingsGenerator):
+    """
+    GenerateFinding is a tool that is utilized to generate findings from a given
+    corpus of content. It is fed a singular resource and the topic to
+    focus/research on.
 
-    def __init__(self, llm: LLM, max_learnings: int = 5):
+    Args:
+        llm (LLM): The LLM to use for the generate finding tool; only used if
+            max_learnings is not provided
+        name (str): The name of the generate finding tool; defaults to
+            "generate_findings"
+        description (str): The description of the generate finding tool;
+            defaults to "Generate findings from a given content and query"
+        max_learnings (int): The maximum number of learnings to generate;
+            defaults to 5
+        id (str): The id of the generate finding tool; defaults to None
+
+    Returns:
+        list[Finding]: A list of findings, which gives a source and important
+            information found within.
+    """
+
+    def __init__(
+        self,
+        llm: LLM,
+        max_learnings: int = 5,
+        name: Optional[str] = None,
+        description: Optional[str] = None,
+        id: Optional[str] = None,
+    ):
+        if description is None:
+            description = "Generate findings from a given content and query"
+
         super().__init__(
             name="generate_findings",
-            description="Generate findings from a given content and query",
+            description=description,
             args=[
                 Argument(
                     "topic",
@@ -218,6 +257,7 @@ class GenerateFinding(FindingsGenerator):
                 + "important information found within.",
                 type="list[Finding]",
             ),
+            id=id,
         )
 
         self.__max_learnings = max_learnings
@@ -279,29 +319,67 @@ class GenerateFinding(FindingsGenerator):
 
 class Researcher(Linear):
     """
-    TODO - write out what this is, how it works, and flow,
-    and do it for the above classes to. This way we can make tests
-    for the flow of it, etc. Then make tests for the abstract classes
-    feature. Then we can move onto deep research
+    Researcher is a tool that is utilized to answer a single question.
+    To do so, it follows the process of:
+
+    1. Generate a set of queries to search for
+    2. Perform the search across a set of Resources
+    3. Judge the relevance of the Resources before consuming them
+    4. Generate Findings from each Resources
+
+    To call the researcher, you simply provide a topic to research. Note that
+    the topic should be specific, detailed, and accurately describe the
+    information desired. It may be wise to utilize an other agent to expand the
+    topic into a more specific question. In short, do *not* treat it like a web
+    search query
+    IE:
+
+    Bad: "stock trading strategies"
+    Good: "Compare common long-term stock trading strategies for average middle
+        class investors, while discussing the potential risks and upsides of
+        each strategy."
+
+    Args:
+        name (str): The name of the researcher; defaults to "researcher"
+        description (str): The description of the researcher
+        llm: The LLM to use for the researcher; only used if query_generator,
+            search_resources, or judge_resources are not provided
+        query_generator: The query generator to use for the researcher;
+            defaults to QueryGenerator(llm)
+        search_resources: The resource search to use for the researcher;
+            this must be provided, as it determines where the researcher
+            will search for information
+        judge_resources: The resource judge to use for the researcher;
+            defaults to DefaultResourceJudge(llm)
+        generating_findings: The findings generator to use for the researcher;
+            defaults to GenerateFinding(llm)
+        max_learnings (int): The maximum number of learnings to generate
+            per resource; defaults to 5
+        max_workers (int): The maximum number of workers to use for the
+            researcher; defaults to 10
+        id (str): The id of the researcher; defaults to None
     """
 
     def __init__(
         self,
-        llm: LLM,
-        description: str,
         name: str = "researcher",
-        query_generator: QueryGenerator = None,
-        search_resources: ResourceSearch = None,
+        description: Optional[str] = None,
+        llm: Optional[LLM] = None,
+        query_generator: Optional[QueryGenerator] = None,
+        search_resources: Optional[ResourceSearch] = None,
         judge_resources: Optional[ResourceJudge] = None,
         generating_findings: Optional[FindingsGenerator] = None,
         max_learnings: int = 5,
         max_workers: int = 10,
-        id: str = None,
+        id: Optional[str] = None,
     ):
-        self._llm = llm
         self._query_generator = query_generator
 
         if judge_resources is None:
+            if llm is None:
+                raise ValueError(
+                    "llm is required if judge_resources is not provided"
+                )
             judge_resources = DefaultResourceJudge(llm)
 
         self._resource_search = ParallelList(
@@ -311,6 +389,10 @@ class Researcher(Linear):
         )
 
         if generating_findings is None:
+            if llm is None:
+                raise ValueError(
+                    "llm is required if generating_findings is not provided"
+                )
             generating_findings = GenerateFinding(llm, max_learnings)
 
         self._finding_generation = ParallelList(
@@ -334,12 +416,21 @@ class Researcher(Linear):
                 description=(
                     "The question to research; ensure you are "
                     "specific, detailed, and concise in asking "
-                    "your question/topic."
+                    "your question/topic. Ask for specifically "
+                    "what you want researched to avoid general "
+                    "responses."
                 ),
                 type="str",
                 required=True,
             ),
         ]
+
+        if description is None:
+            description = (
+                "A researcher that can search for information across a "
+                "set of resources and generate findings from the content "
+                "of those resources."
+            )
 
         super().__init__(
             name,
