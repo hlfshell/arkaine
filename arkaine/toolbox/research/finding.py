7c0db240d9d3a27a2535284a53ce8152b2904954
Keith Chester <kchester@gmail.com>
2025-03-05 16:12:49 -0800

    fix: findings track resources they came from

--- arkaine/toolbox/research/finding.py
+++ arkaine/toolbox/research/finding.py
@@ -1,26 +1,34 @@
 from typing import Any, Dict
 
+from arkaine.utils.resource import Resource
+
 
 class Finding:
 
-    def __init__(self, source: str, summary: str, content: str):
-        self.source = source
+    def __init__(self, resource: Resource, summary: str, content: str):
+        self.source = f"{resource.name} - {resource.source}"
         self.summary = summary
         self.content = content
+        self.resource = resource
 
     def to_json(self):
         return {
-            "source": self.source,
             "content": self.content,
             "summary": self.summary,
+            "resource": self.resource.to_json(),
         }
 
     @classmethod
     def from_json(cls, json: Dict[str, Any]):
-        return cls(json["source"], json["summary"], json["content"])
+        return cls(
+            Resource.from_json(json["resource"]),
+            json["summary"],
+            json["content"],
+        )
 
     def __str__(self):
-        return f"{self.source}\n{self.summary}\n{self.content}"
+        resource = f"{self.resource.name} - {self.resource.source}"
+        return f"{resource}\n{self.summary}\n{self.content}"
 
     def __repr__(self):
         return self.__str__()
