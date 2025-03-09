598050f52d77e385b2b2a7a6e6876d1c90ed9446
Keith Chester <kchester@gmail.com>
2025-03-09 13:17:22 -0700

    feature: WIP - not happy with current abstractiosn
    I keep going back and forth between documents tracking metadata to
    documents being actual helper objects within the tooling. I feel like
    this is an abstractions problem and I need to rethink the flow and use
    cases in order to make this work.
    
    Realistically I'm going to need an Artifact class from which documents
    derive so that I can have generic artifact types, such as Document,
    Image, Podcast, Video, Website, Youtube, etc.

--- arkaine/toolbox/documents.py
+++ arkaine/toolbox/documents.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from concurrent.futures import Future, ThreadPoolExecutor, as_completed
+import json
 import os
 import re
 from abc import ABC, abstractmethod
@@ -6,30 +10,184 @@ import tempfile
 from typing import Dict, List, Optional, Tuple, Union
 
 import google.generativeai as genai
-from PIL import Image
+from PIL import Image as PILImage
+from uuid import uuid4
 
 
-class Document(ABC):
-    def __init__(self):
-        super().__init__()
+class Document:
+    """
+    Document tracks information about a document and its location. All documents
+    are stored in a directory. The file structure is always:
 
-        self.id = None
-        self.content = None
-        self.source = None
-        self.author = None
-        self.title = None
-        self.description = None
-        self.type = None
-        # Keyword tags ("research paper", "legal document", "textbook", "etc")
-        self.tags = []
-        self.__table_of_contents = None
+    <dir_path>/
+        - document.ext      # where ext is pdf, doc, epub, etc
+        - content           # markdown version of the original
+        - *.jpg             # images extracted from the document
+        - metadata.json     # This object as a json file for reloading.
+
+
+    Document has the following fields accessible:
+
+    - id: The unique identifier for the document.
+
+    - location: The path to the directory containing the document, as described
+        above
+
+    - content: The markdown content of the document. This actually loads
+        the content on request, and is not held in memory.
+
+    - authors: The author(s) of the document in a list
+
+    - title: The title of the document
+
+    - description: The description of the document
+
+    - file_type: The extension/filetype of the document - derived if not
+        specified. To be used for readers to know what kind of raw data they
+        may be handling.
+
+    - category: The category of the document, ie "research paper", "legal
+        document", "textbook", etc.
+
+    - tags: The tags of the document. These are N user defined keywords for
+        possible organization/filtering of the document
+
+    - images: [Image] - a list of Image objects associated with the document
+
+    - document: The raw data of the document in question. Loaded on request,
+        not held in memory.
+
+    - filepath: The path to the document's data
+
+    - directory: The directory where all data, the original document, images,
+        and metadata are stored.
+    """
+
+    def __init__(
+        self,
+        library_dir: str,
+        id: str,
+    ):
+        self.__id = id
+        # Ensure the directory for the document exists
+        dir = os.path.join(library_dir, self.__id)
+        if not os.path.exists(dir):
+            raise ValueError("Document not found")
+
+        self.__location = dir
+
+        # Extract the filename from the location provided
+        # IF it exists - it will be document.EXT
+        # List all files and see if we have a document.EXT
+        files = os.listdir(self.__location)
+        # Starts with document?
+        if any(file.startswith("document") for file in files):
+            for file in files:
+                if file.startswith("document"):
+                    try:
+                        self.file_type = os.path.splitext(file)[1]
+                        break
+                    except Exception:
+                        self.file_type = ""
+                        break
+        else:
+            raise ValueError("Document not found")
+
+        # Load the metadata.json which contains all of the other
+        # information
+        metadata_path = os.path.join(self.__location, "metadata.json")
+        if not os.path.exists(metadata_path):
+            raise ValueError("Metadata not found")
+
+        with open(metadata_path, "r") as f:
+            metadata = json.load(f)
+
+        self.authors = metadata["authors"]
+        self.title = metadata["title"]
+        self.description = metadata["description"]
+        self.category = metadata["category"]
+        self.tags = metadata["tags"]
+
+        image_data = metadata["images"]
+        # Each image has a dict of the following form:
+        # {
+        #     "path": str,
+        #     "description": str,
+        #     "page_number": int,
+        #     "id": str,
+        # }
+        self.images = [
+            Image(
+                os.path.join(self.__location, image_data[key]["path"]),
+                image_data[key]["description"],
+                image_data[key]["page_number"],
+                image_data[key]["id"],
+            )
+            for key in image_data
+        ]
+
+    @property
+    def id(self):
+        return self.__id
 
     @property
     def content(self):
-        """
-        todo - if the content is not yet loaded, load it
-        """
-        pass
+        with open(os.path.join(self.location, "content"), "r") as f:
+            return f.read()
+
+    @property
+    def filepath(self):
+        return os.path.join(self.location, f"original.{self.file_type}")
+
+    @property
+    def directory(self):
+        return self.__location
+
+    @property
+    def document(self):
+        with open(self.filepath, "rb") as f:
+            return f.read()
+
+    @classmethod
+    def from_content(
+        self,
+        library_dir: str,
+        filepath: str,
+        content: str,
+        title: str,
+        description: str,
+        category: Optional[str] = None,
+        authors: List[str] = [],
+        tags: List[str] = [],
+        id: Optional[str] = None,
+    ) -> Document:
+        if id is None:
+            id = str(uuid4())
+
+        if id not in os.listdir(library_dir):
+            os.makedirs(os.path.join(library_dir, id))
+
+
+class Image:
+    def __init__(
+        self,
+        path: Union[str, Path],
+        description: str,
+        document: Union[Document, str],
+        page_number: Optional[int] = None,
+        id: Optional[str] = None,
+    ):
+        self.id = id or str(uuid4())
+        self.path = path if isinstance(path, str) else str(path)
+        self.description = description
+        self.document = document if isinstance(document, str) else document.id
+        self.page_number = page_number
+
+    def __str__(self):
+        return self.path
+
+    def load(self) -> PILImage:
+        return PILImage.open(self.path)
 
 
 class DocumentStore(ABC):
@@ -50,6 +208,51 @@ class DocumentStore(ABC):
     # def search(self, query: Query) -> List[Document]:
     #     pass
 
+    def import_from_file(
+        self,
+        filepath: str,
+        pages: Optional[List[Union[int, Tuple[int, int]]]] = None,
+        title: Optional[str] = None,
+        description: Optional[str] = None,
+        category: Optional[str] = None,
+        authors: Optional[List[str]] = None,
+        tags: List[str] = [],
+        ocr: Optional[OCR] = None,
+        id: Optional[str] = None,
+    ) -> Document:
+        """
+        Imports a document from the given filepath. If ocr is provided, the
+        document will be extracted using the ocr tool. If not, it will check
+        to see if a .ocr attribute is present on the implementing class. If
+        not, it will raise an error, otherwise it will utilize that tool.
+
+        Returns a document object, with images attached (if OCR could handle
+        it).
+        """
+        if ocr is None:
+            raise ValueError(
+                "No OCR tool provided or available on "
+                f"{self.__class__.__name__}"
+            )
+
+        text, images, metadata = ocr.extract(filepath, pages)
+
+        document = Document(
+            library_dir="",
+            title=title if title else metadata["title"],
+            description=description if description else metadata["description"],
+            category=category if category else metadata["category"],
+            authors=authors if authors else metadata["authors"],
+            id=id if id else None,
+            content=text,
+            tags=tags,
+        )
+        document.save()
+        for image in images:
+            document.attach_image(image)
+
+        return document
+
 
 class OCR(ABC):
 
@@ -57,18 +260,49 @@ class OCR(ABC):
         super().__init__()
 
     @abstractmethod
-    def extract(self, filepath: str) -> Document:
+    def extract(
+        self,
+        filepath: str,
+        pages: Optional[List[Union[int, Tuple[int, int]]]] = None,
+    ) -> Tuple[str, List[Image], Dict[str, str]]:
+        """
+        Extracts a document from the given filepath by processing it, pulling
+        out the text and images, and returning them as a tuple of text, images,
+        and metadata. metadata is a dict of:
+
+        {
+            "title": str,
+            "authors": List[str],
+            "category": str,
+        }
+
+        Args:
+            filepath: The path to the PDF file to extract
+            pages: The pages to extract from the PDF
+        """
         pass
 
 
 class MarkerOCR(OCR):
+    """
+    MarkerOCR is an OCR tool that converts PDF files to markdown or HTML
+    utilizing Marker (https://github.com/VikParuchuri/marker).
+
+    It requires the use of a Google API key that has access to their generative
+    AI models. Otherwise it runs locally, using GPU acceleration if available.
+
+    MarkerOCR requires two additional dependencies:
+
+    - marker-pdf[full]==1.6.1
+    - pypdf==5.3.1
+    """
 
     def __init__(
         self,
         render_to: str = "markdown",
         api_key: Optional[str] = None,
         convert_images_to_text: bool = True,
-        directory: Optional[str] = None,
+        max_image_transcribe_workers: int = 10,
     ):
         super().__init__()
 
@@ -81,8 +315,6 @@ class MarkerOCR(OCR):
                 "API Key not set - either pass it in or set one of "
                 "GOOGLE_API_KEY or GOOGLE_AISTUDIO_API_KEY"
             )
-
-        self.__directory = directory
 
         self.__convert_images_to_text = convert_images_to_text
         if self.__convert_images_to_text:
@@ -125,6 +357,11 @@ class MarkerOCR(OCR):
             }
         )
 
+        self.__image_transcribe_workers = max_image_transcribe_workers
+        self.__image_transcribe_threadpool = ThreadPoolExecutor(
+            max_workers=self.__image_transcribe_workers
+        )
+
         self.__converter = PdfConverter(
             config=config_parser.generate_config_dict(),
             artifact_dict=create_model_dict(),
@@ -134,30 +371,39 @@ class MarkerOCR(OCR):
         )
 
     def __images_to_text(
-        self, images: Union[str, Path, List[Union[str, Path]]]
-    ) -> str:
-        if isinstance(images, str):
-            images = [images]
-        elif isinstance(images, Path):
-            images = [images]
+        self,
+        images: List[PILImage.Image],
+    ) -> List[str]:
+        descriptions: List[str] = []
 
-        for image_path in images:
-            response = self.__google_model.generate_content(
-                [
-                    "You are an OCR agent. You will transcribe this "
-                    "image (if there is text), then describe it in "
-                    "detail. If it is an equation, write it in latex. "
-                    "If it is a table, write it in markdown",
-                    Image.open(image_path),
-                ]
+        futures: List[Future] = []
+        for image in images:
+            futures.append(
+                self.__image_transcribe_threadpool.submit(
+                    self.__google_model.generate_content,
+                    [
+                        "You are an OCR agent. You will transcribe this "
+                        "image (if there is text), then describe it in "
+                        "detail. If it is an equation, write it in latex. "
+                        "If it is a table, write it in markdown",
+                        image,
+                    ],
+                )
             )
 
-            # Extract and print the description
-            description = response.text
-            return description
+        for future in as_completed(futures):
+            try:
+                response = future.result()
+                description = response.text
+                descriptions.append(description)
+            except Exception as e:
+                print(f"Error transcribing image: {e}")
+                descriptions.append("")
+
+        return descriptions
 
     def __add_image_description_to_tags(
-        self, text: str, images: Dict[str, str]
+        self, text: str, images: List[str], descriptions: List[str]
     ) -> str:
         """
         __image_tags_to_text takes a dict or image keys (filepaths) and block
@@ -186,22 +432,33 @@ class MarkerOCR(OCR):
         The explanations can be *quite* long, so we strip out new lines since
         it's meant for LLM consumption. This can be disabled by turning off the
         images_to_text flag in the class init.
+
+        We are also going to use this opportunity to rename the images to
+        "00000#.jpeg" or equivalent for the image number, since the default
+        names refer to incorrect pages.
         """
-        for key, body in images.items():
+        for index, key in enumerate(images):
+            # Generate the new image name
+            # Isolate the extension of the image
+            extension = os.path.splitext(key)[1]
+            new_name = f"{index:06d}.{extension}"
+
+            description = descriptions[index]
+
             if self.render_to == "markdown":
                 # Ensure the body text is stripped of new lines and any
                 # symbols that could break the markdown parser
                 input = (
-                    body.replace("\n", " ")
+                    description.replace("\n", " ")
                     .replace("[", "\\[")
                     .replace("]", "\\]")
                     .replace('"', '\\"')
                 )
-                text = text.replace(f"![]({key})", f"![{input}]({key})")
+                text = text.replace(f"![]({key})", f"![{input}]({new_name})")
             elif self.render_to == "html":
-                input = body.replace("\n", " ").replace('"', '\\"')
+                input = description.replace("\n", " ").replace('"', '\\"')
                 text = text.replace(
-                    f"<img src={key}>", f"<img src={key} alt={input}>"
+                    f"<img src={key}>", f"<img src={new_name} alt={input}>"
                 )
 
         return text
@@ -458,14 +715,30 @@ class MarkerOCR(OCR):
         return "\n".join(modified_lines)
 
     def extract(
-        self, filepath: str, pages: Optional[List[Union[int, Tuple]]] = None
-    ) -> Document:
+        self,
+        filepath: str,
+        pages: Optional[List[Union[int, Tuple[int, int]]]] = None,
+    ) -> Tuple[str, List[Image], Dict[str, str]]:
         """
         Extracts a document from the given filepath by processing the PDF,
         converting images to text if enabled, and appending page numbers
         to headers. It then uses available metadata (or supplements missing
         data via an LLM based on the table of contents) to populate a Document.
-        Returns a Document instance populated with the content and metadata.
+
+        Args:
+            filepath: The path to the PDF file to extract
+
+            pages: The pages to extract from the PDF
+
+            id: The id of the generated document. If not provided, a uuid will
+                be generated
+
+        Returns:
+            text: The text of the document
+            images: A list of PIL Image objects from the document in order of
+                appearance/id
+            metadata: The metadata of the document, consisting of llm
+                generated title, authors, and category
         """
         if pages is not None:
             with tempfile.TemporaryDirectory() as temp_dir:
@@ -491,42 +764,33 @@ class MarkerOCR(OCR):
             elif self.render_to == "html":
                 text = response.html
 
-        # Save the images to disk.
-        # TODO adjust the image names as they refer to page numbers
-        # that aren't quite right
-        for key, image in response.images.items():
-            # image is a PIL Image object
-            image.save(os.path.join(self.__directory, key))
-
         if self.__convert_images_to_text:
-            image_explanations: Dict[str, str] = {
-                key: self.__images_to_text(
-                    [os.path.join(self.__directory, key)]
-                )
-                for key in response.images.keys()
-            }
-
-            text = self.__add_image_description_to_tags(
-                text, image_explanations
+            image_explanations: List[str] = self.__images_to_text(
+                response.images.values()
             )
 
-        metadata = response.metadata
+            text = self.__add_image_description_to_tags(
+                text, response.images.keys(), image_explanations
+            )
 
         # Extract additional metadata (such as title and author).
-        doc_metadata = self._extract_additional_metadata(text)
+        # Since this is based off of the start of the document, we need
+        # to make sure we have it. Thus if we have pages specified, and it
+        # doesn't include the first page, we need to add it.
+        if pages is not None and 1 not in pages:
+            with tempfile.TemporaryDirectory() as temp_dir:
+                # Get the length of the document
+                reader = self.__pdf_reader(filepath)
+                total_pages = len(reader.pages)
+                modified_file = self.__limit_to_pages(
+                    filepath,
+                    temp_dir,
+                    [(1, min(3, total_pages + 1))],
+                )
+                response = self.__converter(modified_file)
+                beginning_text = response.markdown
+                doc_metadata = self._extract_additional_metadata(beginning_text)
+        else:
+            doc_metadata = self._extract_additional_metadata(text)
 
-        # Create and populate a Document instance.
-        # doc = Document(
-        #     title=doc_metadata.get("title", ""),
-        #     authors=doc_metadata.get("authors", []),
-        #     category=doc_metadata.get("category", ""),
-        #     filepath=filepath,
-        # )
-        # doc.content = text
-        # doc.title = doc_metadata.get("title")
-        # doc.author = doc_metadata.get("author")
-        # doc.source = filepath
-        # # Set other fields (such as tags, type, etc.) as needed.
-
-        # return doc
-        return text, doc_metadata
+        return text, response.images.values(), doc_metadata
